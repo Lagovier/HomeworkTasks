c5b2e669c46fb3b2c7c5b69af7bc8c0b54a1a016
Александр <aleksandr.lagovier@gmail.com>
2024-10-31 23:27:09 +0200

    origin

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2.5, True, 'string'
+print(immutable_var)
+# immutable_var[0]=333
+mutable_list = [1, 3.2, 'красный', False]
+print(mutable_list)
+mutable_list.append(429.123)
+print(mutable_list)
+mutable_list[2]='синий'
+print(mutable_list)
