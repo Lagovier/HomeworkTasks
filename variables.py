5d934937f904763d2205317b0144a23cd7e297aa
Александр <aleksandr.lagovier@gmail.com>
2024-10-31 02:19:24 +0200

    31.10 2:19

---
+++ variables.py
@@ -0,0 +1,5 @@
+homeworks_finished = 12
+hours_total = 1.5
+course_name = "Python"
+hours_per_homework = hours_total / homeworks_finished
+print("Курс:",course_name,", всего задач:",homeworks_finished,", затрачено часов:",hours_total,", среднее время выполнения:",hours_per_homework)
